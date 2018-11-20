3cb0a7d37b5c255668c7cbf229f7745378305524
Blake DeBoer <blakedeboer@users.noreply.github.com>
2018-11-20 12:34:01 -0500

    create csv file and basic parser

---
+++ csv_parser.py
@@ -0,0 +1,11 @@
+import csv
+
+# with open('sports_data.csv', newline='') as csvfile:
+# 	reader = csv.reader(csvfile)
+# 	print('reader', dir(reader))
+
+csv = open('sports_data.csv', newline='')
+
+reader = csv.reader(csvfile)
+
+
